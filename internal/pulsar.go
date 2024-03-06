fc115c4b1d23731a38fb5745cd99153861daa905
Petr Marek <pmarek15@gmail.com>
2024-03-06 20:39:00 +0100

    Add Pulsar support
    Finished Pulsar support. Now enables same benchmarking as Kafka/RedPanda

--- internal/pulsar.go
+++ internal/pulsar.go
@@ -1,6 +1,14 @@
 package internal
 
-import "github.com/apache/pulsar-client-go/pulsar"
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+)
 
 func ConnectPulsar(url string, numberOfMessages int, sizeOfMessage int) {
     client, err := pulsar.NewClient(pulsar.ClientOptions{
@@ -11,6 +19,45 @@ func ConnectPulsar(url string, numberOfMessages int, sizeOfMessage int) {
         panic(err)
     }
 
-
     defer client.Close()
+
+
+    producer, err := client.CreateProducer(pulsar.ProducerOptions{
+        Topic: "myTopic",
+    })
+
+    if err != nil {
+        panic(err)
+    }
+
+    defer producer.Close()
+
+    value := make([]byte, sizeOfMessage)
+    rand.Read(value)
+
+
+    messagesSent := 0
+
+    ctx := context.Background()
+    start := time.Now()
+    
+    for i := 0; i < numberOfMessages; i++ {
+        msg := pulsar.ProducerMessage{
+            Payload: value,
+        }
+
+        producer.SendAsync(ctx, &msg, func(mi pulsar.MessageID, pm *pulsar.ProducerMessage, err error) {
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            messagesSent++
+        })
+    }
+
+    elapsed := time.Since(start)
+
+    messagesPerSecond := (float64(numberOfMessages) / elapsed.Seconds())
+    throughput := (float64(numberOfMessages) * float64(sizeOfMessage)) / elapsed.Seconds()
+    fmt.Printf("Time taken [s]: %f\nMessages/s: %f\nThroughput [MB/s]: %.2f\n", elapsed.Seconds(), messagesPerSecond, throughput / 1000000)
 }
